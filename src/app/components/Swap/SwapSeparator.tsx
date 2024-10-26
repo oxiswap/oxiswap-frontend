5511ce191035b805199fdbdc0afd82bbf10f4e1c
0x0077 <zbsqx0597@gmail.com>
2024-10-26 21:27:39 +0800

    update separator button

--- src/app/components/Swap/SwapSeparator.tsx
+++ src/app/components/Swap/SwapSeparator.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 
-const SwapSeparator = () => {
+const SwapSeparator = ({ onClick }: { onClick: () => void }) => {
+
   return (
-    <div className="absolute left-1/2 top-1/3 mt-12 transform -translate-x-1/2 -translate-y-1/2 z-10">
+    <button className="absolute left-1/2 top-1/3 mt-12 transform -translate-x-1/2 -translate-y-1/2 z-10" onClick={onClick}>
       <div className="bg-oxi-bg-02 border-4 border-white rounded-lg p-1.5">
-        <Image src="/swap-separator.svg" alt="separator" width={16} height={16} />
+        <Image src="/swap-separator.svg" alt="separator" width={16} height={16} className="hover:rotate-180 transition-all duration-300" />
       </div>
-    </div>
+    </button>
   );
 };
 
